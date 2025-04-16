5c1a861d1d44efa588d1e2266fd5e2091cde76e8
That0neGuy1 <163476246+That0neGuy1@users.noreply.github.com>
2025-04-15 22:18:03 -0700

    Code comments pt.3

--- Assets/Scripts/IUseable Floor.cs
+++ Assets/Scripts/IUseable Floor.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//<summary>
+// Interface for gameObjects that can be interacted with, shared required functions
+//</summary>
 public interface IUseableFloor
 {
     void Use(GameObject source);
